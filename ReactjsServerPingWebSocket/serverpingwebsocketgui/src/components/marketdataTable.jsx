63354d20843ddd4e70e9413d699849cb48ecc71a
reuvab <reuven.aboulker@gmail.com>
2020-07-15 15:50:41 +0800

    Migrated table to material ui table

--- ReactjsServerPingWebSocket/serverpingwebsocketgui/src/components/marketdataTable.jsx
+++ ReactjsServerPingWebSocket/serverpingwebsocketgui/src/components/marketdataTable.jsx
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 
-import Table from './common/table';
 import AsyncSubsribeField from './asyncSubscribeField';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import TableCell from '@material-ui/core/TableCell';
 
-class MarketDataTable extends Component {
-  rounding = 2;
-  columns = [
+import { TableSortLabel } from '@material-ui/core';
+import MarketDataTableCell from './marketDataTableCell/MarketDataTableCell';
+
+export default function MarketDataTable({ dataSourceLst }) {
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('_id');
+
+  const onRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const columns = [
     { path: '_id', label: 'Identifier' },
     {
       path: 'price',
@@ -14,17 +31,69 @@ class MarketDataTable extends Component {
     },
   ];
 
-  render() {
-    const { dataSourceLst, onSort, sortColumn } = this.props;
-    return (
-      <Table
-        columns={this.columns}
-        data={dataSourceLst}
-        sortColumn={sortColumn}
-        onSort={onSort}
-      ></Table>
-    );
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
   }
-}
 
-export default MarketDataTable;
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell>
+                <TableSortLabel
+                  active={orderBy === column.path}
+                  direction={orderBy === column.path ? order : 'asc'}
+                  onClick={createSortHandler(column.path)}
+                >
+                  {column.label}
+                  {orderBy === column.path ? (
+                    <span hidden>
+                      {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                    </span>
+                  ) : null}
+                </TableSortLabel>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {stableSort(dataSourceLst, getComparator(order, orderBy)).map((item) => (
+            <TableRow key={item._id}>
+              {columns.map((column) => (
+                <MarketDataTableCell item={item} column={column} />
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
