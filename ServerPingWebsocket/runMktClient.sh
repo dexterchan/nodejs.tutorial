6a73f7fca27f1fd038f7d087b8edd07b98c41cf3
dexterchan <dexterchan@gmail.com>
2020-07-16 12:02:55 +0800

    updated runMktClient.sh to receive argument dynamically

--- ServerPingWebsocket/runMktClient.sh
+++ ServerPingWebsocket/runMktClient.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-node client/serverpingclient.js --protocol=wss --hostname=treequery.org --port=443
+#node client/serverpingclient.js --protocol=wss --hostname=treequery.org --port=443
+arg=""
+for var in "$@"
+do
+    arg="$arg $var"
+done
+
+node client/serverpingclient.js $arg
